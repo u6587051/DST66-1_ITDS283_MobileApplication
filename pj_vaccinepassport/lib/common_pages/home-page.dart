2eece0532956638ab326caa499f3db6f26778cee
thanaguide <thanawich.jue@student.mahidol.edu>
2024-04-03 04:02:13 +0700

    homepage v2

--- pj_vaccinepassport/lib/common_pages/home-page.dart
+++ pj_vaccinepassport/lib/common_pages/home-page.dart
@@ -1,50 +1,76 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://via.placeholder.com/150'), // Your profile picture
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {
-                // Add your notification action here
-              },
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0, 1],
+                  colors: [
+                    Color.fromARGB(255, 45, 71, 55),
+                    Color.fromARGB(255, 124, 150, 112),
+                  ],
+                ),
+              ),
             ),
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () {
-                // Add your exit action here
-              },
+            AppBar(
+              backgroundColor: Colors.transparent, // Make app bar transparent
+              elevation: 0, // Remove app bar shadow
+              leading: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 0, 10),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://via.placeholder.com/150'), // Your profile picture
+                ),
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    // Add your notification action here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.exit_to_app, color: Colors.white),
+                  onPressed: () {
+                    // Add your exit action here
+                  },
+                ),
+              ],
             ),
           ],
         ),
-        body: Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [0, 0.45, 0.75, 0.88],
-              colors: [
-                Color.fromARGB(255, 45, 71, 55),
-                Color.fromARGB(255, 124, 150, 112),
-                Color.fromARGB(255, 176, 173, 140),
-                Color.fromARGB(255, 238, 230, 222)
-              ],
-            ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0, 0.45, 0.75, 0.88],
+            colors: [
+              Color.fromARGB(255, 45, 71, 55),
+              Color.fromARGB(255, 124, 150, 112),
+              Color.fromARGB(255, 176, 173, 140),
+              Color.fromARGB(255, 238, 230, 222),
+            ],
           ),
-          child: Row(
-            children: [],
-            
-          ),
-        ));
+        ),
+        child: Text('Your body content here'),
+      ),
+    );
   }
 }
