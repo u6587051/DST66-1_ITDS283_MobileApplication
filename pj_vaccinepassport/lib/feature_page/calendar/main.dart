45882cbe9093cff6be33aed2c3fbc2e5336fc8db
Jittakan Damrongtrakoonwat <108443635+u6587051@users.noreply.github.com>
2024-04-03 02:12:02 +0700

    test

---
+++ pj_vaccinepassport/lib/feature_page/calendar/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pj_vaccinepassport/pages/Login_Page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    return const MaterialApp(
+      title: 'Vaccine Passport',
+      debugShowCheckedModeBanner: false,
+      home: LogInPage()
+    );
+  }
+}
